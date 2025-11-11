13cf7d52bc4f21a6ed62a9db81f586bce0d6f3fb
lai2-24 <lai2-24@itismeucci.com>
2025-11-11 11:24:36 +0100

    first commit

---
+++ src/main/java/server/Main.java
@@ -0,0 +1,11 @@
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(3000);
+    }
+}
